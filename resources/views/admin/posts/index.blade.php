2d2079bd5bc4353fc0136d7a779bdccd70e96fe7
TommasoBernardo <tommasob1210@gmail.com>
2023-02-21 09:31:42 +0100

    update

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>prova</h1>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController as DashboardController;
+use App\Http\Controllers\Admin\PostController as PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -20,6 +22,7 @@ Route::get('/', function () {
 
 Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('/posts', PostController::class);
 });
 
 Route::middleware('auth')->group(function () {
