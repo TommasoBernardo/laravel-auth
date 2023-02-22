98d99bcd734a220b40741d332378b23379014952
TommasoBernardo <tommasob1210@gmail.com>
2023-02-22 19:07:25 +0100

    order by date

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -25,7 +25,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::paginate(15);
+        $posts = Post::orderBy('date', 'DESC')->paginate(15);
         return view('admin.posts.index', compact('posts'));
     }
 
