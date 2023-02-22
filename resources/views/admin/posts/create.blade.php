00c6052f17a5ed346d9348e2ddc1b4c3a85598c1
TommasoBernardo <tommasob1210@gmail.com>
2023-02-22 11:00:52 +0100

    delte form and add @include

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -2,37 +2,6 @@
 
 @section('content')
     <div class="container mt-5">
-        <form action="{{ route('admin.posts.store') }}" method="POST">
-            @csrf
-
-            <h4 class="fw-bold">
-                Autore: {{ Auth::user()->name }}
-            </h4>
-            <div class="mb-3">
-                <label for="title" class="form-label">title</label>
-                <input type="text" class="form-control" id="title" placeholder="the lord of kings" name="title">
-            </div>
-
-            <div class="mb-3">
-                <label for="date" class="form-label">data</label>
-                <input type="date" class="form-control" id="date" rows="15" name="date"></input>
-            </div>
-
-            <div class="mb-3">
-                <label for="content" class="form-label">Example textarea</label>
-                <textarea class="form-control" id="content" rows="15" name="content"></textarea>
-            </div>
-
-            <div class="mb-3">
-                <button type="submit" class="btn btn-info">
-                    submit your film
-                </button>
-            </div>
-        </form>
-
-        <div>
-            <a href="{{ route('admin.posts.index') }}" class="btn btn-primary">Back</a>
-        </div>
-
+        @include('admin.partials.editCreate', ['method' => 'POST', 'routeAddress' => 'admin.posts.store'])
     </div>
 @endsection
